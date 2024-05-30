0594ce4a39a8f75b5915f223eefde66bb21f33a0
mdhadi89 <muhammadhadi1989@gmail.com>
2024-05-30 22:44:10 +0800

    Eclipse Project

---
+++ eclipse-workspace/hadiLoginApp/src/main/java/com/mvc/controller/RestrictedServlet.java
@@ -0,0 +1,19 @@
+package com.mvc.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class RestrictedServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+	 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{ 
+	 		System.out.println("Welcome to the Restricted Page!");
+			request.getRequestDispatcher("Manager.jsp").forward(request, response);
+	}
+}
